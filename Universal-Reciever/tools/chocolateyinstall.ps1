73269c8cd81da82facea46e4d5a0aee92eb701e1
fabiovalverde <42446709+fabiovalverde@users.noreply.github.com>
2020-11-16 15:11:50 -0300

    Initial commit

---
+++ Universal-Reciever/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  softwareName   = 'Universal-Reciever*'
+  url          = 'https://dl.dell.com/FOLDER03428757M/10/APP_IO_W7_W8_W10_A00_Setup-VMRGG_ZPE.exe'
+  checksum       = 'fffec57d023c32a59f9c9691b0532506'
+  checksumType   = 'MD5'
+  silentArgs     = "/S"
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
